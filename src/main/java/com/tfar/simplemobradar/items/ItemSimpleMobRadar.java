1080bc24f4d4ec0e65fb853a1cb6c466dec63b28
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-02-25 02:43:21 -0500

    fixed a bug where items weren't working

--- src/main/java/com/tfar/simplemobradar/items/ItemSimpleMobRadar.java
+++ src/main/java/com/tfar/simplemobradar/items/ItemSimpleMobRadar.java
@@ -5,6 +5,7 @@ import com.tfar.simplemobradar.config.ConfigHandler;
 import com.tfar.simplemobradar.init.ModItems;
 import com.tfar.simplemobradar.util.IHasModel;
 import com.tfar.simplemobradar.util.Reference;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -33,21 +34,17 @@ public class ItemSimpleMobRadar extends Item implements IHasModel {
     }
 
     private static int r = ConfigHandler.RANGE_MOBS;
-    private List<Entity> entityList;
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
 
         BlockPos pos = player.getPosition();
-
         if (!world.isRemote && !player.isSneaking()) {
             player.getCooldownTracker().setCooldown(this, 20);
-
-            entityList = getMobList(world, pos, player, hand);
-            player.getHeldItem(hand).getTagCompound().setInteger("Size", entityList.size());
-            if (entityList != null && entityList.size() > 0) {
+            int size = getMobs(world, pos, player, hand);
+            player.getHeldItem(hand).getTagCompound().setInteger("Size", size);
+            if (size>0) {
                 player.getHeldItem(hand).getTagCompound().setInteger("State", 1);
-                getClosestMobs(player, hand);
             } else {
                 player.getHeldItem(hand).getTagCompound().setInteger("State", 0);
             }
@@ -61,22 +58,19 @@ public class ItemSimpleMobRadar extends Item implements IHasModel {
         return 720000;
     }
 
-    public List<Entity> getMobList(World Worldin, BlockPos pos, EntityPlayer player, EnumHand hand) {
+    public int getMobs(World Worldin, BlockPos pos, EntityPlayer player, EnumHand hand) {
 
         int x = pos.getX();
         int y = pos.getY();
         int z = pos.getZ();
 
-        return Worldin.getEntitiesWithinAABB(Reference.mob_class.get(player.getHeldItem(hand).getTagCompound().getInteger("mob type")), new AxisAlignedBB(x - r, y - r, z - r, x + r, y + r, z + r));
-    }
-
-    public void getClosestMobs(EntityPlayer player, EnumHand hand) {
-        List<Entity> entities = entityList;
+        List<Entity> entities = Worldin.getEntitiesWithinAABB(Reference.mob_class.get(player.getHeldItem(hand).getTagCompound().getInteger("mob type")), new AxisAlignedBB(x - r, y - r, z - r, x + r, y + r, z + r));
+        if (entities.size()==0)return 0;
         int closest_mob = 0;
         double distance;
         double closest = 2000000000;
         for (int i = 0; i < entities.size(); i++) {
-            ((EntityLivingBase) entityList.get(i)).addPotionEffect(new PotionEffect(MobEffects.GLOWING, 400, 1));
+            ((EntityLivingBase) entities.get(i)).addPotionEffect(new PotionEffect(MobEffects.GLOWING, 400, 1));
             distance = player.getDistance(entities.get(i).posX, entities.get(i).posY, entities.get(i).posZ);
             if (distance < closest) {
                 closest = distance;
@@ -86,6 +80,7 @@ public class ItemSimpleMobRadar extends Item implements IHasModel {
         player.getHeldItem(hand).getTagCompound().setDouble("X position", entities.get(closest_mob).posX);
         player.getHeldItem(hand).getTagCompound().setDouble("Y position", entities.get(closest_mob).posY);
         player.getHeldItem(hand).getTagCompound().setDouble("Z position", entities.get(closest_mob).posZ);
+    return entities.size();
     }
 
     @Override
@@ -97,6 +92,7 @@ public class ItemSimpleMobRadar extends Item implements IHasModel {
         stack.setTagCompound(new NBTTagCompound());
         stack.getTagCompound().setInteger("mob type", 0);
         stack.getTagCompound().setInteger("State", 2);
+        stack.getTagCompound().setInteger("Size", 0);
     }
 
     public int getState(ItemStack stack) {
