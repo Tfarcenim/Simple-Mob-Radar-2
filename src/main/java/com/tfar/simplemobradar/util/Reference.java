0987bed30170c3da5e4dd60db2c23ed05d3e296e
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-02-26 01:45:27 -0500

    changed version to 2.1

--- src/main/java/com/tfar/simplemobradar/util/Reference.java
+++ src/main/java/com/tfar/simplemobradar/util/Reference.java
@@ -1,7 +1,6 @@
 package com.tfar.simplemobradar.util;
 
 import net.minecraft.entity.Entity;
-import net.minecraftforge.fml.common.registry.EntityEntry;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,17 +9,13 @@ public class Reference {
 
         public static final String MOD_ID = "simplemobradar";
         public static final String NAME = "Simple Mob Radar";
-        public static final String VERSION = "2.0";
+        public static final String VERSION = "2.1";
         public static final String MC_VERSION = "1.12.2,";
         public static final String CLIENT_PROXY_CLASS = "com.tfar.simplemobradar.proxy.ClientProxy";
         public static final String COMMON_PROXY_CLASS = "com.tfar.simplemobradar.proxy.CommonProxy";
 
-
-
         public static List<String> sorted_mobs = new ArrayList<>();
         public static List<Class<? extends Entity>> sorted_mob_class = new ArrayList<>();
-
-
 
         public static List<String> sorted_animals = new ArrayList<>();
         public static List<Class<? extends Entity>> sorted_animal_class = new ArrayList<>();
