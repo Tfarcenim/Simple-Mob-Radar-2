3a99d314faf76f049fd2422d70df49dd1edbf269
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-02-27 08:29:58 -0500

    cleanup

--- src/main/java/com/tfar/simplemobradar/util/ItemUtils.java
+++ src/main/java/com/tfar/simplemobradar/util/ItemUtils.java
@@ -1,8 +1,10 @@
 package com.tfar.simplemobradar.util;
 
+import com.tfar.simplemobradar.MainClass;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 public class ItemUtils {
 
@@ -10,31 +12,19 @@ public class ItemUtils {
         return player.getHeldItemMainhand();
     }
 
-    public static String getMobName(ItemStack stack){
-        return I18n.format("entity." + Reference.mobs.get(stack.getTagCompound().getInteger("mobtype")).getName()+ ".name");
+    public static String getMobName(ItemStack stack) {
+        return I18n.format("entity." + Reference.mobs.get(stack.getTagCompound().getInteger("mobtype")).getName() + ".name");
     }
 
-    public static String getPassiveName(ItemStack stack){
-        return I18n.format("entity." + Reference.animals.get(stack.getTagCompound().getInteger("mobtype")).getName()+ ".name");
+    public static String getPassiveName(ItemStack stack) {
+        return I18n.format("entity." + Reference.animals.get(stack.getTagCompound().getInteger("mobtype")).getName() + ".name");
     }
 
-    public static int getDistance(EntityPlayer player, ItemStack stack){
-        return (int)player.getDistance( stack.getTagCompound().getDouble("X position"), stack.getTagCompound().getDouble("Y position"),  stack.getTagCompound().getDouble("Z position"));
+    public static int getDistance(EntityPlayer player, ItemStack stack) {
+        return (int) player.getDistance(stack.getTagCompound().getDouble("X position"), stack.getTagCompound().getDouble("Y position"), stack.getTagCompound().getDouble("Z position"));
     }
 
-    public static int getTotal(ItemStack stack){
-        return  stack.getTagCompound().getInteger("Size");
+    public static int getTotal(ItemStack stack) {
+        return stack.getTagCompound().getInteger("Size");
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
